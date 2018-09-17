8de09f4609c9c11a69bb1cde62d3ef70800e0ec7
jameslee1187 <jameslee1187@gmail.com>
2018-09-17 05:25:43 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -2,6 +2,13 @@ def dictionary
   dictionary = {
     "hello" => "hi"
     "to" => "2"
-    
+    "two" => "2"
+    "too" => "2"
+    "for" => "4"
+    "four" => "4"
+    "be" => "b"
+    "you" => "u"
+    "at" => "@"
+    "and" => "&"
   }
 # Write your code here.

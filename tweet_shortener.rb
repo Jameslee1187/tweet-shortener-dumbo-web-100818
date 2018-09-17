5d49b3af8536e7b8934960237a9442b2b69ccca4
jameslee1187 <jameslee1187@gmail.com>
2018-09-17 05:50:44 +0000

    Done.

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,54 @@
-# Write your code here.
+def dictionary
+  dictionary = {
+    "hello" => "hi",
+    "to" => "2",
+    "two" => "2",
+    "too" => "2",
+    "for" => "4",
+    "four" => "4",
+    "be" => "b",
+    "you" => "u",
+    "at" => "@",
+    "and" => "&"
+  }
+end
+
+tweet1 = "Hey guys, can anyone teach me how to be cool? I really want to be the best at everything, you know what I mean? Tweeting is super fun you guys!!!!"
+tweet2 = "OMG you guys, you won't believe how sweet my kitten is. My kitten is like super cuddly and too cute to be believed right?"
+tweet3 = "I'm running out of example tweets for you guys, which is weird, because I'm a writer and this is just writing and I tweet all day. For real, you guys. For real."
+tweet4 = "GUISEEEEE this is so fun! I'm tweeting for you guys and this tweet is SOOOO long it's gonna be way more than you would think twitter can handle, so shorten it up you know what I mean? I just can never tell how long to keep typing!"
+tweet5 = "New game. Middle aged tweet followed by #youngPeopleHashTag Example: Gotta get my colonoscopy and mammogram soon. Prevention is key! #swag"
+tweets = [tweet1, tweet2, tweet3, tweet4, tweet5]
+
+def word_substituter(string)
+  string.split.collect do |word|
+    if dictionary.keys.include?(word.downcase)
+      word = dictionary[word.downcase]
+    else
+      word
+    end
+  end.join(" ")
+end
+word_substituter(tweet1)
+
+def bulk_tweet_shortener(tweets)
+  tweets.each do |tweet|
+    puts word_substituter(tweet)
+  end
+end
+    
+def selective_tweet_shortener(tweets)
+  if tweets.length > 140
+    word_substituter(tweets)
+  else
+    tweets
+  end
+  end
+  
+def shortened_tweet_truncator(tweet3)
+  if word_substituter(tweet3).length > 140 
+    word_substituter(tweet3)[0..136] + '...'
+  else 
+    tweet3 
+  end 
+end 

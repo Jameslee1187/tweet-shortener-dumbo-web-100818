96d6227239fc71c6ef648518b08760b8f7f5bbfb
jameslee1187 <jameslee1187@gmail.com>
2018-09-17 05:26:52 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -12,5 +12,6 @@ def dictionary
     "and" => "&"
   }
 end
-tweet1 = 
-# Write your code here.
+
+tweet1 = "Hey guys, can anyone teach me how to be cool? I really want to be the best at everything, you know what I mean? Tweeting is super fun you guys!!!!"
+tweet2 = 
